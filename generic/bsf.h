a90adedde0261b212b82a9b350c15c65deec245b
Alister Chowdhury <krax.powah@gmail.com>
2021-03-30 08:43:29 +0100

    Updating bsf and test_members_are_equal

--- generic/bsf.h
+++ generic/bsf.h
@@ -7,42 +7,32 @@
 inline
 unsigned int bsf(unsigned int m)
 {
-
 #if defined(__GNUC__) || defined(__CLANG__)
     return __builtin_ctz(m);
-
 #elif defined(_MSC_VER)
     unsigned long v = 0;
     _BitScanForward(&v, (unsigned long)m);
     return v;
-
 #else
     unsigned int value = 0;
     while (m >>= 1) ++value;
     return value;
-
 #endif
-
 }
 
 
 inline
-unsigned long long bsf(unsigned long long m)
+unsigned long bsf(unsigned long long m)
 {
-
 #if defined(__GNUC__) || defined(__CLANG__)
-    return __builtin_ctzl(m);
-
+    return (unsigned long)__builtin_ctzl(m);
 #elif defined(_MSC_VER)
-    unsigned long long v = 0;
-    _BitScanForward64(&v, (unsigned long)m);
+    unsigned long v = 0;
+    _BitScanForward64(&v, m);
     return v;
-
 #else
     unsigned long long value = 0;
     while (m >>= 1) ++value;
-    return value;
-
+    return (unsigned long)value;
 #endif
-
 }
--- generic/test_members_are_equal.h
+++ generic/test_members_are_equal.h
@@ -17,6 +17,15 @@ template<typename... Ts> using void_t = typename make_void<Ts...>::type;
 #endif
 
 
+#if defined(__GNUC__)
+    #define CONSTEXPR_ALWAYS_INLINE     __attribute__((always_inline)) constexpr inline
+#elif defined(_MSC_VER)
+    #define CONSTEXPR_ALWAYS_INLINE     constexpr __forceinline
+#else
+    #define CONSTEXPR_ALWAYS_INLINE     constexpr inline
+#endif
+
+
 template <typename T>
 struct is_simple_type {
     constexpr static bool value = (
@@ -34,52 +43,52 @@ struct cmp_eq_one_impl {
 private:
     // For simple types compare them in the 'light' block
     template<typename U=T, std::enable_if_t<is_simple_type<U>::value, bool> = true>
-    constexpr static bool cmp_eq_light_impl(const T& a, const T& b) {
+    CONSTEXPR_ALWAYS_INLINE static bool cmp_eq_light_impl(const T& a, const T& b) {
         return a == b;
     }
     template<typename U=T, std::enable_if_t<!is_simple_type<U>::value, bool> = true>
-    constexpr static bool cmp_eq_light_impl(const T& a, const T& b) {
+    CONSTEXPR_ALWAYS_INLINE static bool cmp_eq_light_impl(const T& a, const T& b) {
         return true;
     }
 
     // For everything else compare them in the 'normal' block
     template<typename U=T, std::enable_if_t<is_simple_type<U>::value, bool> = true>
-    constexpr static bool cmp_eq_normal_impl(const T& a, const T& b) {
+    CONSTEXPR_ALWAYS_INLINE static bool cmp_eq_normal_impl(const T& a, const T& b) {
         return true;
     }
     template<typename U=T, std::enable_if_t<!is_simple_type<U>::value, bool> = true>
-    constexpr static bool cmp_eq_normal_impl(const T& a, const T& b) {
+    CONSTEXPR_ALWAYS_INLINE static bool cmp_eq_normal_impl(const T& a, const T& b) {
         return a == b;
     }
 
     // Helper for a.size() == b.size() compares
     template <typename U=T, typename=void_t<>>
     struct cmp_eq_size_impl {
-        constexpr static bool call(const U&, const U&) {
+        CONSTEXPR_ALWAYS_INLINE static bool call(const U&, const U&) {
             return true;
         }
     };
 
     template <typename U>
     struct cmp_eq_size_impl<U, void_t<decltype( std::declval<const U&>().size() )> > {
-        constexpr static bool call(const U& a, const U& b) {
+        CONSTEXPR_ALWAYS_INLINE static bool call(const U& a, const U& b) {
             return a.size() == b.size();
         }
     };
 
-    constexpr static bool cmp_eq_size(const T& a, const T& b) {
+    CONSTEXPR_ALWAYS_INLINE static bool cmp_eq_size(const T& a, const T& b) {
         return cmp_eq_size_impl<T>::call(a, b);
     }
 
 
 public:
-    constexpr static bool cmp_eq_light(const T& a, const T& b) {
+    CONSTEXPR_ALWAYS_INLINE static bool cmp_eq_light(const T& a, const T& b) {
         return (
             cmp_eq_light_impl<>(a, b)
             && cmp_eq_size(a, b)
         );
     }
-    constexpr static bool cmp_eq_normal(const T& a, const T& b) {
+    CONSTEXPR_ALWAYS_INLINE static bool cmp_eq_normal(const T& a, const T& b) {
         return (
             cmp_eq_normal_impl<>(a, b)
         );
@@ -95,22 +104,22 @@ private:
     // all_true helper
     template <typename...>
     struct all_true_impl {
-        constexpr static bool call(...) { return true; }
+        CONSTEXPR_ALWAYS_INLINE static bool call(...) { return true; }
     };
     template <typename BoolT, typename... NextBoolsT>
     struct all_true_impl<BoolT, NextBoolsT...> {
-        constexpr static bool call(const BoolT current, const NextBoolsT... nexts) {
+        CONSTEXPR_ALWAYS_INLINE static bool call(const BoolT current, const NextBoolsT... nexts) {
             return current & all_true_impl<NextBoolsT...>::call(nexts...);
         }
     };
     template <typename... NextBoolsT>
-    constexpr static bool all_true(const NextBoolsT... bools) {
+    CONSTEXPR_ALWAYS_INLINE static bool all_true(const NextBoolsT... bools) {
         return all_true_impl<NextBoolsT...>::call(bools...);
     }
 
 
 public:
-    constexpr static bool call(const Ts&... a_values, const Ts&... b_values) {
+    CONSTEXPR_ALWAYS_INLINE static bool call(const Ts&... a_values, const Ts&... b_values) {
         return (
             // First do light compares
             all_true(cmp_eq_one_impl<Ts>::cmp_eq_light(a_values, b_values)...)
